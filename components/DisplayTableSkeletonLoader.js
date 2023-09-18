3fbb16e49cc55012947ff6f7a3b25317a3d70faa
devhasibulislam <devhasibulislam@gmail.com>
2023-09-18 22:23:24 +0600

    integrate table skeleton loader while fetching table data

---
+++ components/DisplayTableSkeletonLoader.js
@@ -0,0 +1,45 @@
+/**
+ * Title: Write a program using JavaScript on DisplayTableSkeletonLoader
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 18, September 2023
+ */
+
+import React from "react";
+
+const DisplayTableSkeletonLoader = () => {
+  return (
+    <>
+      <tr className="animate-pulse">
+        <td className="px-6 py-4 whitespace-no-wrap w-40">
+          <div className="bg-gray-200 h-[45px] w-[85.71px] rounded-lg"></div>
+        </td>
+        <td className="px-6 py-4 whitespace-no-wrap">
+          <div className="bg-gray-200 h-4 w-full mb-2"></div>
+          <div className="bg-gray-200 h-4 w-1/2"></div>
+        </td>
+        <td className="px-6 py-4 whitespace-no-wrap">
+          <div className="bg-gray-200 h-4 w-full mb-2"></div>
+          <div className="bg-gray-200 h-4 w-1/2"></div>
+        </td>
+        <td className="px-6 py-4 whitespace-no-wrap w-40">
+          <div className="bg-gray-200 h-4 w-full mb-2"></div>
+          <div className="bg-gray-200 h-4 w-1/2"></div>
+        </td>
+        <td className="px-6 py-4 whitespace-no-wrap text-right">
+          <div className="bg-gray-200 h-[45px] w-[45px] rounded"></div>
+        </td>
+      </tr>
+    </>
+  );
+};
+
+export default DisplayTableSkeletonLoader;
