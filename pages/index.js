04f011ed44d8120ce851d4549399e84308b2cc7b
devhasibulislam <devhasibulislam@gmail.com>
2023-09-18 20:16:02 +0600

    integrate twitter card credentials

--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,31 @@ export default function Home() {
       className={`h-screen w-screen overflow-y-auto overflow-x-hidden ${inter.className} p-4`}
     >
       <Head>
+        {/* Meta Data Credentials */}
         <title>URL Shortener - Free</title>
         <meta
           name="description"
           content="Checkout our free tool that short your url with encryption"
           key="desc"
         />
-        <meta property="og:url" content="https://lisonet-template.vercel.app/" />
+
+        {/* Twitter Card Credentials */}
+        <meta
+          property="twitter:image"
+          content="https://res.cloudinary.com/dho0rpn5a/image/upload/v1695044502/samples/url-shortener_h4oa18.png"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="Try URL Shortener for Free" />
+        <meta
+          property="twitter:description"
+          content="Checkout our free tool that short your url with encryption"
+        />
+
+        {/* Open Graph Credentials */}
+        <meta
+          property="og:url"
+          content="https://lisonet-template.vercel.app/"
+        />
         <meta property="og:title" content="Try URL Shortener for Free" />
         <meta
           property="og:description"
