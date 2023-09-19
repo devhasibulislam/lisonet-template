bda327b3868f5647502f5ff453e1cd0e2450b870
devhasibulislam <devhasibulislam@gmail.com>
2023-09-19 09:32:43 +0600

    integrate redirect

--- pages/api/[slug].js
+++ pages/api/[slug].js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
       }
 
       // Return the data if found
-      res.status(200).json({ message: "Data fetched successfully", data });
+      res.redirect(data.metaUrl);
     } catch (error) {
       // Handle any potential errors
       console.error(error);
