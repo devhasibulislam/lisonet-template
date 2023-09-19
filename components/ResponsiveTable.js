8cda22affca0b40f1ce4a3703106ed2b986f85a9
devhasibulislam <devhasibulislam@gmail.com>
2023-09-19 09:12:21 +0600

    integrate refresh button

--- components/ResponsiveTable.js
+++ components/ResponsiveTable.js
@@ -22,7 +22,7 @@ const ResponsiveTable = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [tableData, setTableData] = useState([]);
 
-  useEffect(() => {
+  const fetchData = () => {
     setIsLoading(true);
 
     fetch("/api/getAllData")
@@ -34,10 +34,39 @@ const ResponsiveTable = () => {
       .catch((error) => {
         console.error(error);
       });
+  };
+
+  useEffect(() => {
+    fetchData();
   }, []);
 
   return (
     <section className="overflow-x-auto">
+      <button
+        onClick={() => {
+          setIsLoading(true);
+          setTableData([]);
+          fetchData();
+        }}
+        className="flex flex-row gap-x-1 items-center text-xs border border-black px-1.5 py-1 rounded-full hover:bg-black hover:text-white duration-100 transition-all"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="w-4 h-4"
+        >
+          <path
+            fillRule="evenodd"
+            d="M15.312 11.424a5.5 5.5 0 01-9.201 2.466l-.312-.311h2.433a.75.75 0 000-1.5H3.989a.75.75 0 00-.75.75v4.242a.75.75 0 001.5 0v-2.43l.31.31a7 7 0 0011.712-3.138.75.75 0 00-1.449-.39zm1.23-3.723a.75.75 0 00.219-.53V2.929a.75.75 0 00-1.5 0V5.36l-.31-.31A7 7 0 003.239 8.188a.75.75 0 101.448.389A5.5 5.5 0 0113.89 6.11l.311.31h-2.432a.75.75 0 000 1.5h4.243a.75.75 0 00.53-.219z"
+            clipRule="evenodd"
+          />
+        </svg>
+        Refresh
+      </button>
+
+      <hr className="my-4" />
+
       <table className="min-w-full">
         <thead>
           <tr>
