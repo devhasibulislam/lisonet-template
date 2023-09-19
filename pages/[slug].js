88d9e8d18ef53607a00b95ec4beaa9aed7cc11ee
devhasibulislam <devhasibulislam@gmail.com>
2023-09-19 09:46:07 +0600

    init error segment

--- pages/[slug].js
+++ pages/[slug].js
@@ -15,11 +15,19 @@
 
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 
 const MetaSlug = () => {
   const router = useRouter();
-  console.log(router?.query?.slug);
+  const slug = router?.query?.slug;
+
+  useEffect(() => {
+    fetch(`/api/${slug}`)
+      .then((req) => req.json())
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  }, [slug]);
 
   return (
     <section className="w-screen h-screen overflow-hidden flex justify-center items-center">
